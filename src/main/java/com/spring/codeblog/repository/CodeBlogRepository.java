931ed71e7a33c24f72d53ff7d36b0ef27f5744c5
IsaacOB <iasa>
2022-03-02 10:25:57 -0300

    Create Service and ServiceImpl for blog

--- src/main/java/com/spring/codeblog/repository/CodeBlogRepository.java
+++ src/main/java/com/spring/codeblog/repository/CodeBlogRepository.java
@@ -3,5 +3,5 @@ package com.spring.codeblog.repository;
 import com.spring.codeblog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BlogRepository extends JpaRepository<Post, Long> {
+public interface CodeBlogRepository extends JpaRepository<Post, Long> {
 }
