9dd92a2f831b3e0bd396bb1b398aafe166df05c2
IsaacOB <iasa>
2022-03-05 09:01:19 -0300

    getPostDetails implementation

--- src/main/java/com/spring/codeblog/controller/CodeBlogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeBlogController.java
@@ -5,6 +5,7 @@ import com.spring.codeblog.service.CodeBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -20,6 +21,14 @@ public class CodeBlogController {
         ModelAndView modelAndView = new ModelAndView("posts");
         List<Post> postList = codeBlogService.findAll();
         modelAndView.addObject("posts", postList);
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/posts/{id}")
+    public ModelAndView getPostDetails(@PathVariable long id) {
+        ModelAndView modelAndView = new ModelAndView("postDetails");
+        Post post = codeBlogService.findById(id);
+        modelAndView.addObject("post", post);
         return modelAndView;
     }
 }
